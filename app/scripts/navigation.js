a0aadccceaa4660ca9254b4b964c6887aa320f96
Branimir Valentin <branimir.valentin.reroot@gmail.com>
2019-11-25 20:32:13 +0100

    feat(mobile): modified all sections for mobile devices, feat(navigation): hidden hero and locked body when navigation is active

--- app/scripts/navigation.js
+++ app/scripts/navigation.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
   var $menu = $("#menu"),
-    $menulink = $(".menu-link");
+    $menulink = $(".menu-link"),
+    $hero = $(".hero"),
+    $body = $("body");
 
   $menulink.click(function() {
     $menulink.toggleClass("active");
     $menu.toggleClass("active");
+    $hero.toggleClass("hide");
+    $body.toggleClass("lock");
     return false;
   });
 });
